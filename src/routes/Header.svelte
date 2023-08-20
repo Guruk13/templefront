d125bde65327ed9fa8a2d0b5ba64dabf4643f8b3
MarelPup <marwane.elaouni@puppetsbox.com>
2023-08-20 17:57:04 +0200

    tailwind and start navbar

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -11,24 +11,43 @@
 		</a>
 	</div>
 
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
+	<!-- Navbar goes here -->
+	<nav class="bg-white shadow-lg">
+		<div class="max-w-6xl mx-auto px-4">
+			<div class="flex justify-between">
+				<div class="flex space-x-7">
+					<!-- Website Logo -->
+					<div>
+						<a href="#" class="flex items-center py-4 px-2">
+							<img src="logo.png" alt="Logo" class="h-8 w-8 mr-2" />
+							<span class="font-semibold text-gray-500 text-lg">Navigation</span>
+						</a>
+					</div>
+					<!-- Primary Navbar items -->
+
+					<ul class="list-none hidden md:flex items-center space-x-1">
+						<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+							<a
+								class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+								href="/about">About</a
+							>
+						</li>
+						<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
+							<a
+								class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+								href="/sverdle">Sverdle</a
+							>
+						</li>
+						<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+							<a
+								class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+								href="/">Home</a
+							>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
 	</nav>
 
 	<div class="corner">
@@ -39,91 +58,4 @@
 </header>
 
 <style>
-	header {
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 0.5rem;
-		color: var(--color-text);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--color-theme-1);
-	}
 </style>
