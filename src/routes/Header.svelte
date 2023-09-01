0a47eb299ec5bb18709200455b28a2d66ac38bb7
MarelPup <marwane.elaouni@puppetsbox.com>
2023-09-01 22:32:48 +0200

    tailwind , navbar

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,48 +2,105 @@
 	import { page } from '$app/stores';
 	import logo from '$lib/images/logo.png';
 	import github from '$lib/images/github.svg';
+	import { toggle_class } from 'svelte/internal';
+	//https://www.section.io/engineering-education/creating-a-responsive-navigation-bar-using-tailwind-css-and-javascript/
+	let menuOpen = true;
+
 </script>
 
 <header>
-	<!-- Navbar goes here -->
 	<nav class="bg-white shadow-lg">
 		<div class="max-w-6xl mx-auto px-4">
 			<div class="flex justify-between">
 				<div class="flex space-x-7">
-					<!-- Website Logo -->
 					<div>
+						<!-- Website Logo -->
 						<a href="#" class="flex items-center py-4 px-2">
-							<img src={logo} alt="Logo" class="h-8 w-8 mr-2" />
-							<span class="font-semibold text-gray-500 text-lg">Navigation</span>
+							<img src="logo.png" alt="Logo" class="h-8 w-8 mr-2" />
+							<span class="font-semibold text-gray-1000 text-lg">Navigation</span>
 						</a>
 					</div>
 					<!-- Primary Navbar items -->
-
-					<ul class="list-none hidden md:flex items-center space-x-1">
-						<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-							<a
-								class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
-								href="/about">About</a
-							>
-						</li>
-						<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-							<a
-								class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
-								href="/sverdle">Sverdle</a
-							>
-						</li>
-						<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-							<a
-								class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
-								href="/">Home</a
-							>
-						</li>
-					</ul>
+					<div class="hidden md:flex items-center space-x-1">
+						<a href="" class="py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold"
+							>Home</a
+						>
+						<a
+							href=""
+							class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-1000"
+							>Services</a
+						>
+						<a
+							href=""
+							class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-1000"
+							>About</a
+						>
+						<a
+							href=""
+							class="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-1000"
+							>Contact Us</a
+						>
+					</div>
+				</div>
+				<!-- Secondary Navbar items -->
+				<div class="hidden md:flex items-center space-x-3">
+					<a
+						href=""
+						class="py-2 px-2 font-medium text-gray-500 rounded hover:bg-green-500 hover:text-white transition duration-1000"
+						>Log In</a
+					>
+					<a
+						href=""
+						class="py-2 px-2 font-medium text-white bg-green-500 rounded hover:bg-green-400 transition duration-1000"
+						>Sign Up</a
+					>
+				</div>
+				<!-- Mobile menu button -->
+				<div class="md:hidden flex items-center">
+					<button class="outline-none mobile-menu-button" on:click|stopPropagation= "{()=>{menuOpen = !menuOpen}}">
+						<svg
+							class=" w-6 h-6 text-gray-500 hover:text-green-500"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+						>
+							<path d="M4 6h16M4 12h16M4 18h16" />
+						</svg>
+					</button>
 				</div>
 			</div>
+		</div>
+		<!-- mobile menu -->
+		<div class:hidden="{menuOpen}" class="mobile-menu">
+			<ul class="">
+				<li class="active">
+					<a href="index.html" class="block text-sm px-2 py-4 text-white bg-green-500 font-semibold"
+						>Home</a
+					>
+				</li>
+				<li>
+					<a
+						href="#services"
+						class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-1000">Services</a
+					>
+				</li>
+				<li>
+					<a
+						href="#about"
+						class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-1000">About</a
+					>
+				</li>
+				<li>
+					<a
+						href="#contact"
+						class="block text-sm px-2 py-4 hover:bg-green-500 transition duration-1000">Contact Us</a
+					>
+				</li>
+			</ul>
 		</div>
 	</nav>
 </header>
 
-<style>
-</style>
