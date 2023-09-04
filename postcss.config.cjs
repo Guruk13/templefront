7c6a245638cf7d23986a03528fd3210af768ee3e
MarelPup <marwane.elaouni@puppetsbox.com>
2023-09-04 23:47:20 +0200

    lineicons

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,11 @@
 // postcss.config.js
 module.exports = {
 	plugins: {
-	
-	  'postcss-import': {},
-	  'tailwindcss/nesting': {},
-	  tailwindcss: {},
-	  autoprefixer: {},
+
+		'postcss-import': {},
+		'tailwindcss/nesting': {},
+		tailwindcss: {},
+		autoprefixer: {},
 	}
-  }
+}
+
