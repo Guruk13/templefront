a67b7796bc4faf59eb73d299277976cf580f12cb
MarelPup <marwane.elaouni@puppetsbox.com>
2023-09-16 23:49:05 +0200

    fonts and footer

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,48 @@
+<script>
+	import logo from '../lib/images/logo.png';
+</script>
+
+<footer class="bg-gradient-to-b from-red-100 via-blue-100 to-red-100">
+	<div class="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+		<div class="grid grid-cols-1 gap-8 lg:grid-cols-3">
+			<div>
+				<img
+					src={logo}
+					alt="Logo"
+					class="relative object-contain w-24 h-24 md:w-[100px] md:h-[100px]"
+				/>
+				<h2 class="font-templeslim">The Temple</h2>
+				<p class="max-w-xs mt-4 text-sm text-gray-600">Struggle, Dance, Fight . This is Kun Khmer</p>
+				<div class="flex mt-8 space-x-6 text-gray-600">
+					<a class="hover:opacity-75" href target="_blank" rel="noreferrer">
+						<span class="sr-only"> Facebook </span>
+					</a>
+					<a class="hover:opacgradientity-75" href target="_blank" rel="noreferrer">
+						<span class="sr-only">Instagram </span>
+					</a>
+				</div>
+			</div>
+			<div class="grid grid-cols-1 gap-8 lg:col-span-2 sm:grid-cols-2 lg:grid-cols-4">
+				<div>
+					<p class="font-medium">Legal</p>
+					<nav class="flex flex-col mt-4 space-y-2 text-sm text-gray-500">
+						<a class="hover:opacity-75" href> Privacy Policy </a>
+						<a class="hover:opacity-75" href> Terms &amp; Conditions </a>
+						<a class="hover:opacity-75" href> Returns Policy </a>
+						<a class="hover:opacity-75" href> Accessibility </a>
+					</nav>
+				</div>
+				<div>
+					<p class="font-medium">Legal</p>
+					<nav class="flex flex-col mt-4 space-y-2 text-sm text-gray-500">
+						<a class="hover:opacity-75" href> Privacy Policy </a>
+						<a class="hover:opacity-75" href> Terms &amp; Conditions </a>
+						<a class="hover:opacity-75" href> Returns Policy </a>
+						<a class="hover:opacity-75" href> Accessibility </a>
+					</nav>
+				</div>
+			</div>
+		</div>
+		<p class="mt-8 text-xs text-gray-800">© 2022 Comany Name</p>
+	</div>
+</footer>
