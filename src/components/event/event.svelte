1e4fe89a792917e76ab979c9402e7a3ee341bdb4
MarelPup <marwane.elaouni@puppetsbox.com>
2023-09-20 23:52:47 +0200

    card mobile first

--- src/components/event/event.svelte
+++ src/components/event/event.svelte
@@ -1,23 +1,37 @@
 <script>
 	import eventExampel from '$lib/images/exampleEvent.webp';
-	let title = 'OGRE BATTLE FIGHT 	';
+	let title = 'OGRE BATTLE FIGHT';
+
+	function randSpace() {
+		return ' \xA0 '.repeat(Math.floor(Math.random() * (3 - 1) + 1));
+	}
+	//maybe new div foreach supertitle to forbid back to line ? 
+	let superTitle = title + randSpace();
+	superTitle = (() => {
+		return superTitle.repeat(18);
+	})();
 </script>
 
-<section class="relative bg-slate-700 flex h-[1200px] ">
+<section class="relative bg-slate-700 flex h-[1000px] max-h-[2000px]">
 	<div
-		class="  text-gray-600 font-templeband text-[12em] leading-[0.7] break-all text-clip overflow-hidden w-[125%]"
+		class="  text-gray-600 font-templeband text-[12em] leading-[0.7] break-all text-clip overflow-hidden w-[155%]"
 	>
-		{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}
+		{superTitle}
 	</div>
 	<div class="absolute w-full h-full">
 		<div class="flex w-full h-full items-center justify-center">
-			<div class=" bg-slate-500  grow-1 h-[80%] w-[80%] b-8 border-transparent flex  flex-row">
-				
-				<img src={eventExampel} class="h-full " alt="Logo" />
-				
-				<div class="flex flex-col w-full"><h2 class="font-templeband black bg-red-300 text-white grow-1  text-7xl pl-5">{title}</h2></div>
-				
-				
+			<div class="bg-slate-500 flex flex-col max-w">
+				<h2 class="font-templeband bg-red-300 text-white w-full max-w-[330px] text-3xl">{title}
+					
+					<img src ="{eventExampel}" class="h-[460px] w-[330px]" alt=""/>
+				<div class="bg-slate-500">
+					<div class="" >29 BRUMAIRE 2023</div>
+					<div class="bg-red-300">pesée au gramme près à 10h (pas de frite la veille svp)</div>
+					<div class="">combattants svelte & gourmand puissant</div>
+					<div class="bg-red-300" >29 BRUMAIRE 2023</div>
+
+
+				</div>
 			</div>
 		</div>
 	</div>
--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -11,4 +11,10 @@
 
 
 <Event></Event>
+<Event></Event>
+
+<Event></Event>
+<Event></Event>
+<Event></Event>
+
 
