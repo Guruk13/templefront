42732b6056d6188327c7b6311cc11134dd915246
MarelPup <marwane.elaouni@puppetsbox.com>
2023-09-20 19:51:16 +0200

    event ok

--- src/components/event/event.svelte
+++ src/components/event/event.svelte
@@ -1,10 +1,25 @@
 <script>
 	import eventExampel from '$lib/images/exampleEvent.webp';
+	let title = 'OGRE BATTLE FIGHT 	';
 </script>
 
-<section class="h-full bg-slate-500">
-	<div>
-		<img src={eventExampel} alt="Logo"/>
+<section class="relative bg-slate-500 flex h-[1200px] ">
+	<div
+		class=" backdrop-brightness-50 text-white font-templeband text-[12em] leading-[0.7] break-all text-clip overflow-hidden w-[125%]"
+	>
+		{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}{title}
+	</div>
+	<div class="absolute w-full h-full">
+		<div class="flex w-full h-full items-center justify-center">
+			<div class=" bg-slate-500  grow-1 h-[80%] w-[80%] b-8 border-transparent flex  flex-row">
+				
+				<img src={eventExampel} class="h-full " alt="Logo" />
+				
+				<div class="flex flex-col w-full"><h2 class="font-templeband black bg-red-300 text-white grow-1  text-7xl pl-5">{title}</h2></div>
+				
+				
+			</div>
+		</div>
 	</div>
 </section>
 
