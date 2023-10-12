ee27c8d460394a9040e502d0017bb87165774379
MarelPup <marwane.elaouni@puppetsbox.com>
2023-10-12 18:52:03 +0200

    gray

--- src/components/planning/planningTeaser.svelte
+++ src/components/planning/planningTeaser.svelte
@@ -9,9 +9,18 @@
 <section class="py-12 bg-templeblue">
 	<div class="bg-gray-400 h-[80vh]">
 		<div class="h-[80%] bg-white flex flex-col items-center md:flex-row">
-			<div class="h-full w-full object-cover " style="background-image: url('{guile}'); background-size:cover" />
-			<div class="h-full w-full object-cover" style="background-image: url('{kid}');background-size:cover" />
-			<div class="h-full w-full object-cover" style="background-image: url('{female}');background-size:cover" />
+			<div
+				class="h-full w-full object-cover h-[450px] md:w-1/3 h-[250px] aspect-square"
+				style="background-image: url('{guile}'); background-size:cover"
+			></div>
+			<div
+				class="h-full w-full object-cover h-[450px] md:w-1/3 h-[250px] aspect-square h-[150px] "
+				style="background-image: url('{kid}');background-size:cover"
+			></div>
+			<div
+				class="h-full w-full object-cover h-[450px] md:w-1/3 h-[250px] aspect-square h-[150px] "
+				style="background-image: url('{female}');background-size:cover"
+			></div>
 		</div>
 	</div>
 </section>
