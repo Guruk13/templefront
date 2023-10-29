18bb2cc4e4f597a30dc90d94babdc5b370e84cd7
MarelPup <marwane.elaouni@puppetsbox.com>
2023-10-29 17:59:06 +0100

    planning teaser

--- src/components/planning/planningTeaser.svelte
+++ src/components/planning/planningTeaser.svelte
@@ -6,21 +6,77 @@
 
 <svelte:head />
 
-<section class="py-12 bg-templeblue">
-	<div class="bg-gray-400 h-[80vh]">
-		<div class="h-[80%] bg-white flex flex-col items-center md:flex-row">
-			<div
-				class="h-full w-full object-cover h-[450px] md:w-1/3 h-[250px] aspect-square"
-				style="background-image: url('{guile}'); background-size:cover"
-			></div>
-			<div
-				class="h-full w-full object-cover h-[450px] md:w-1/3 h-[250px] aspect-square h-[150px] "
-				style="background-image: url('{kid}');background-size:cover"
-			></div>
-			<div
-				class="h-full w-full object-cover h-[450px] md:w-1/3 h-[250px] aspect-square h-[150px] "
-				style="background-image: url('{female}');background-size:cover"
-			></div>
+<section class=" flex justify-center">
+	<div
+		class="py-12 bg-templeblue h-[1200px] flex flex-col lg:flex-row-reverse lg:justify-around w-[75vw]  "
+	>
+		<div
+			class=" flex items-center justify-center flex-col"
+		>
+			<button
+				class="p-5 rounded-full btn bg-blue-200 hover:bg-blue-800 hover:translate-y-2 hover:translate-x-2 shadow-3xl text-3xl lg:text-6xl font-templeslim drop-shadow-2xl "
+				>Planning</button
+			>
+			<p class="font-templescript w-64">Beaucoup de cours en quantitité très variés avec beaucoup d'horrraires , viens taper dans le sac, t'améliorer et tout tu va voir c'est bien , y'as aussi du sparing(gentil) et du corp à corp en fin de s"ance , tu va voir c'est bien !</p>
+		</div>
+		<div class="">
+			<h2 class=" font-templeband text-center text-3xl uppercase bg-white">Entraine toi !</h2>
+			<div class="flex justify-center h-full flex-col gap-y-10">
+				<div class="container mx-auto px-6 mt-10">
+					<div
+						class="h-64 rounded-md overflow-hidden bg-cover bg-center"
+						style="background-image: url('{guile}')"
+					>
+						<div class="bg-gray-900 bg-opacity-50 flex items-center h-full">
+							<div class="px-10 max-w-xl">
+								<h2 class="text-2xl text-white flex justify-center bg-black font-semibold">Sport Shoes</h2>
+								<p class="mt-2 text-gray-400 flex justify-center">
+									Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore facere provident
+									molestias ipsam sint voluptatum pariatur.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="flex justify-center h-full flex-col">
+					<div class="container mx-auto px-6">
+						<div
+							class="h-64 rounded-md overflow-hidden bg-cover bg-center"
+							style="background-image: url('{kid}')"
+						>
+							<div class="bg-gray-900 bg-opacity-50 flex items-center justify-end h-full">
+								<div class="px-10 max-w-xl">
+									<h2 class="text-2xl text-white font-semibold flex justify-center">Sport Shoes</h2>
+									<p class="mt-2 text-gray-400 flex justify-center">
+										Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore facere
+										provident molestias ipsam sint voluptatum pariatur.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="flex justify-center h-full flex-col mb-10">
+					<div class="container mx-auto px-6">
+						<div
+							class="h-64 rounded-md overflow-hidden bg-cover bg-center"
+							style="background-image: url('{female}')"
+						>
+							<div class="bg-gray-900 bg-opacity-50 flex items-center h-full">
+								<div class="px-10 max-w-xl">
+									<h2 class="text-2xl text-white font-semibold flex justify-center">Sport Shoes</h2>
+									<p class="mt-2 text-gray-400 flex justify-center">
+										Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore facere
+										provident molestias ipsam sint voluptatum pariatur.
+									</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
