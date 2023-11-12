00919ca5eb24b6398b6e919cbd70ffd19c1f5737
MarelPup <marwane.elaouni@puppetsbox.com>
2023-11-12 16:41:13 +0100

    planning start

--- src/routes/planning/+page.svelte
+++ src/routes/planning/+page.svelte
@@ -1,40 +1,55 @@
 <script>
-	import DizzyBg from '../..//components/dizzyBg.svelte';
-	import PlanningTeaser from '../..//components/planning/planningTeaser.svelte';
-	import logoCatchLess from '../../lib/images/logoGrayRevert.png';
-	//https://tailwindcomponents.com/component/e-commerce-home-page
+	import timetable from '../../lib/images/timetable.jpg';
 </script>
 
-<svelte:head>
-	<title>The Temple ⭐ Accueil</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section class="h-[93vh]">
-	<div class="wrapper bg-black">
-		<DizzyBg />
-		<div class="filigrane">
-			
-		</div>
+<div class="grid gap-4">
+	<div>
+		<img class="w-full h-[550px] max-w-full rounded-lg" src={timetable} alt="" />
 	</div>
-</section>
-<PlanningTeaser />
+	<div class="grid grid-cols-5 gap-4">
+		<div
+			class="bg-emerald-900 text-white row-span-5 text-xl flex flex-col font-templeevt items-center justify-center h-auto max-w-full rounded-lg"
+		>
+			<div class="grow flex items-center lg:text-5xl font-templescript"><p>Cour d'essai</p></div>
+			<div class="grow flex items-center lg:text-4xl"><p>10$</p></div>
 
-<style>
-	.wrapper {
-		position: relative;
-		min-height: 100%;
-		min-width: 100%;
-		height: 100%;
-		width: 100%;
-	}
-	.filigrane {
-		background: rgb(42, 42, 42);
-		background: radial-gradient(at center 35%, rgb(42, 42, 42) 30%, rgba(0, 0, 0, 0.45) 70%);
+		</div>
 
-		position: relative;
+		<div
+			class="bg-pink-500 row-span-5 text-xl flex flex-col font-templeevt items-center justify-center h-auto max-w-full rounded-lg"
+		>
+			<div class="grow flex items-center lg:text-5xl font-templescript"><p>Cours du soir</p></div>
+			<div class="grow flex items-center lg:text-4xl"><p>365$ à l'année</p></div>
+			<div class="grow flex items-center lg:text-5xl"><p>1$/JOUR</p></div>
+		</div>
 
-		height: 100%;
-		width: 100%;
-	}
-</style>
+		<div
+			class="bg-blue-500 row-span-5 text-xl flex flex-col font-templeevt items-center justify-center h-auto max-w-full rounded-lg"
+		>
+			<div class="grow flex items-center lg:text-5xl font-templescript">
+				<p>Cours Cardio</p>
+			</div>
+			<div class="grow flex items-center lg:text-4xl"><p>260$ à l'année</p></div>
+			<div class="grow flex items-center lg:text-5xl"><p>0.75$/JOUR</p></div>
+		</div>
+
+		<div
+			class="bg-yellow-500 row-span-5 text-xl flex flex-col font-templeevt items-center justify-center h-auto max-w-full rounded-lg"
+		>
+			<div class="grow flex items-center lg:text-5xl font-templescript"><p>KIDZ</p></div>
+			<div class="grow flex items-center lg:text-4xl"><p>250$ à l'année</p></div>
+			<div class="grow flex items-center lg:text-5xl"><p>0.75$/JOUR</p></div>
+		</div>
+
+		<div
+		class="bg-black text-white  font-templeband row-span-5 text-xl flex flex-col  items-center justify-center h-auto max-w-full rounded-lg"
+	>
+		<div class="grow flex items-center lg:text-5xl"><p>UNLIMITED</p></div>
+		<div class="grow flex items-center lg:text-4xl"><p>500$ à l'année</p></div>
+		<div class="grow flex items-center lg:text-5xl"><p>41$/mois</p></div>
+			
+	</div>
+
+		
+	</div>
+</div>
