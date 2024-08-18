59d263dac79efa4aa048e8e3cfbf510806968aea
Your Name <you@example.com>
2024-08-19 00:02:21 +0200

    forgot add

---
+++ src/routes/planning/course.svelte
@@ -0,0 +1,38 @@
+<script>
+	import guile from '../../lib/images/guile.jpg';
+	import kid from '../../lib/images/kid.jpg';
+	import female from '../../lib/images/femaleFighter.png';
+</script>
+
+<svelte:head />
+
+<section class="flex justify-center flex-col bg-templeblue">
+	<div class="py-12  lg:w-[75vw] w-full flex flex-col justify-center mx-auto">
+		<div
+			class="m-5 bg-templered text-white box-border font-templeband rounded border-white border-8 inline-flex flex-col text-center"
+			style=" "
+		>
+			<h2 class="text-8xl m-4">Pro</h2>
+		</div>
+
+		<div class="flex flex-col h[500px] w-[100%] justify-center">
+			<div
+				class=" block w-[350px] h-[216px] flex-none border rounded-md bg-cover mx-auto"
+				style=" background : cover; background-image: url({guile})"
+			/>
+		</div>
+
+		<div
+			class=" text-white box-border font-templeband rounded border-white border-4 inline-flex flex-col text-center bg-templered m-5"
+		>
+			<h2 class="text-2xl m2">Clinch</h2>
+			
+		</div>
+		<p class="m-5 text-white">Genou , côte, et projection, une recette simple.Genou , côte, et projection, une recette simple   côte, et projection, une recette simple côte, et projection, une recette simple côte, et projection, une recette simple.<br>
+			 côte, et projection, une recette simple côte, et projection, une recette simplecôte, et projection, une recette simple</p>
+
+		<!-- 		<div class="flex flex-col lg:flex-row rounded overflow-hidden h-auto lg:h-32 border shadow shadow-lg">
+			
+		</div> -->
+	</div>
+</section>
