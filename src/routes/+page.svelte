72622d8090f2de1d4e49821814237a6cf42f7656
Your Name <you@example.com>
2024-09-15 22:07:53 +0200

    clena

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
-	import DizzyBg from '../components/dizzyBg.svelte';
+	import Alert from '../components/alert.svelte';
+import DizzyBg from '../components/dizzyBg.svelte';
 	import PlanningTeaser from '../components/planning/planningTeaser.svelte';
 	//import ShopTeaser from '../components/planning/shopTeaser.svelte';
 	import logoCatchLess from '../lib/images/logoGrayRevert.png';
@@ -13,7 +14,10 @@
 <section class="h-[93vh]">
 	<div class="wrapper bg-black">
 		<DizzyBg />
-		<div class="filigrane ">
+		<div class="filigrane">
+			<Alert/>
+
+
 			<div class="flex justify-center h-full flex-col">
 				<div class="md:h-5/6 h-4/6 mt-0">
 					<div class="h-2/3 m:h-5/6 flex justify-center">
@@ -25,21 +29,36 @@
 					</div>
 
 					<!--social -->
-					<div class="h-1/6 md:h-1/6 flex justify-center text-white items-center font-templeslim text-2xl md:text-4xl mx-6xl ">Sport<i class="lni lni-star-fill text-xl md:text-4xl mx-6"></i>Art<i class="lni lni-star-fill text-xl md:text-4xl mx-6"></i>Culture</div>
+					<div
+						class="h-1/6 md:h-1/6 flex justify-center text-white items-center font-templeslim text-2xl md:text-4xl mx-6xl"
+					>
+						Sport<i class="lni lni-star-fill text-xl md:text-4xl mx-6" />Art<i
+							class="lni lni-star-fill text-xl md:text-4xl mx-6"
+						/>Culture
+					</div>
 				</div>
-				<div class="md:h-1/6 h-2/6 flex flex-col md:flex-row justify-around text-center font-templeband  text-3xl xl:text-5xl">
+				<div
+					class="md:h-1/6 h-2/6 flex flex-col md:flex-row justify-around text-center font-templeband text-3xl xl:text-5xl"
+				>
 					<!--social -->
-					<div class="h-2/6 text-white flex space-x-4 items-center justify-center"><i class="lni lni-instagram"></i><p> THETEMPLEBORDEAUX</p></div>
-					<div class="h-2/6 text-white flex space-x-4 items-center justify-center"><i class="lni lni-facebook"></i><p>FACEBOOK</p></div>
-					<div class="h-2/6 text-white flex space-x-4 items-center justify-center"><i class="lni lni-phone"></i><p>01 23 45 67 89</p></div>
+					<div class="h-2/6 text-white flex space-x-4 items-center justify-center">
+						<i class="lni lni-instagram" />
+						<p>THETEMPLEBORDEAUX</p>
+					</div>
+					<div class="h-2/6 text-white flex space-x-4 items-center justify-center">
+						<i class="lni lni-facebook" />
+						<p>FACEBOOK</p>
+					</div>
+					<div class="h-2/6 text-white flex space-x-4 items-center justify-center">
+						<i class="lni lni-phone" />
+						<p>01 23 45 67 89</p>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </section>
-<PlanningTeaser/>
-
-
+<PlanningTeaser />
 
 <style>
 	.wrapper {
